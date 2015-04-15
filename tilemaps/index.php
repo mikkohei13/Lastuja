a0f5da98ef594ac15c021809ace6f813b97385ab
Mikko Heikkinen <hawk@biomi.org>
2015-04-15 23:34:30 +0300

    Zoom settings, text edits

--- tilemaps/index.php
+++ tilemaps/index.php
@@ -33,7 +33,7 @@
 		var mapquestOSM = new L.TileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
 			maxZoom: 17,
 			subdomains: ["otile1", "otile2", "otile3", "otile4"],
-			attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+			attribution: 'Tiles <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Map data <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors CC-BY-SA'
 		});
 
 		var mbTiles = new L.tileLayer('mbtiles.php?db=Espoo-hakkuut3.mbtiles&z={z}&x={x}&y={y}', {
@@ -44,15 +44,16 @@
 
 		map = new L.Map("map",{
 			zoom: 13,
+			maxZoom: 16,
             center: [60.235864, 24.642099],
 			layers: [mapquestOSM, mbTiles]
 		});
 
 		var baseLayers = {
-			"MapQuest Streets": mapquestOSM
+			"Taustakartta": mapquestOSM
 		};
 		var overlays = {
-			"Local MBTiles File": mbTiles,
+			"Hakkuukartat": mbTiles,
 		};
 
 		layersControl = new L.Control.Layers(baseLayers, overlays, {
