1d92d2906bbfc1d07eae8f42c856ca955893f4fe
Mikko Heikkinen <hawk@biomi.org>
2015-10-01 23:30:55 +0300

    Basic image

--- trains/index.php
+++ trains/index.php
@@ -8,13 +8,29 @@ $xml = simplexml_load_string($xmlString);
 foreach($xml->channel->item as $train)
 {
 	$id = (string) $train->guid;
-	$trains[$id]['latlon'] = (string) $train->georss_point;
+	$latlon = explode(" ", (string) $train->georss_point);
+	$trains[$id]['lat'] = $latlon[0];
+	$trains[$id]['lon'] = $latlon[1];
 	$trains[$id]['speed'] = (int) $train->speed;
 	$trains[$id]['from'] = (string) $train->from;
 	$trains[$id]['to'] = (string) $train->to;
 }
 
-echo "<pre>";
-print_r ($trains);
+// echo "<pre>"; print_r ($trains); exit(); // debug
 
+$im = imagecreate(500, 500);
 
+$background_color = imagecolorallocate($im, 0, 200, 0);
+
+// choose a color for the ellipse
+$ellipseColor = imagecolorallocate($im, 0, 255, 255);
+
+foreach($trains as $id => $arr)
+{
+	// draw the ellipse
+	imagefilledellipse($im, $arr['lat'], $arr['lon'], 10, 10, $ellipseColor);
+}
+
+header("Content-Type: image/png");
+imagepng($im);
+imagedestroy($im);
