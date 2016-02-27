ffbb29dd7a8b905cf93393fe5cb9ad610ba95bf2
Mikko Heikkinen <hawk@biomi.org>
2016-02-27 22:08:54 +0200

    You Don't Know JS -practice

---
+++ javascript/ch2.php
@@ -0,0 +1,18 @@
+﻿<pre>
+BEGIN
+<script>
+"use strict";
+
+var g = "globaali";
+
+function test()
+{
+	console.log( this.g ); // globaali in non-strict mode, undefined in strict mode, because ... ?
+}
+
+test();
+
+console.log( this.g );
+
+</script>
+END
