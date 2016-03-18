7f11a3dd62dcdc9d6c0c797cde1dc350ac3c4995
Mikko Heikkinen <hawk@biomi.org>
2016-03-18 23:35:55 +0200

    Results as HTML table

--- miljoonaponttoa/yle.php
+++ miljoonaponttoa/yle.php
@@ -12,9 +12,19 @@ $tringaMuni['Helsinki'] = 1;
 $tringaMuni['Vantaa'] = 1;
 $tringaMuni['Sipoo'] = 1;
 $tringaMuni['Pornainen'] = 1;
+$tringaMuni['Kerava'] = 1;
+$tringaMuni['Tuusula'] = 1;
+$tringaMuni['Järvenpää'] = 1;
+$tringaMuni['Nurmijärvi'] = 1;
+$tringaMuni['Hyvinkää'] = 1;
 $tringaMuni['Mäntsälä'] = 1;
+$tringaMuni['Vihti'] = 1;
+$tringaMuni['Karkkila'] = 1;
 $tringaMuni['Kauniainen'] = 1;
 $tringaMuni['Kirkkonummi'] = 1;
+$tringaMuni['Lohja'] = 1;
+$tringaMuni['Siuntio'] = 1;
+$tringaMuni['Inkoo'] = 1;
 $tringaMuni['Raasepori'] = 1;
 $tringaMuni['Hanko'] = 1;
 // more...
@@ -33,10 +43,13 @@ foreach ($arr as $key => $muniItem)
 
 arsort($municipalities);
 
-echo "<pre>";
+$i = 1;
+echo "<table id='miljoonaponttoa' class='basictable'>\n";
 foreach ($municipalities as $muni => $count)
 {
-	echo $muni . "\t" . $count . "\n";
+	echo "<tr><td>" . $i . ".</td><td>" . $muni . "</td><td>" . $count . "</td></tr>\n";
+	$i++;
 }
+echo "</table>\n";
 
 
