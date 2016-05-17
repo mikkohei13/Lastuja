77063b5fca97df13749eb995cc5168fd8fdf50b3
Mikko Heikkinen <hawk@biomi.org>
2016-05-17 12:48:18 +0300

    Init gridcreator

---
+++ gridcreator/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Tool that creates a KML grid file
+
+Input:
+- lat & lon of the SW corner of the gridded area (decimal degrees)
+- Lat & lon of the NE corner of the gridded area (decimal degrees)
+- Grid size (decimal degrees)
+
+*/
+
+
+
