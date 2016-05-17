c43ae7032b115ae168f5840bc02ea328db6e9f86
Mikko Heikkinen <hawk@biomi.org>
2016-05-17 12:59:53 +0300

    Horizontal line

--- gridcreator/index.php
+++ gridcreator/index.php
@@ -10,5 +10,30 @@ Input:
 
 */
 
+// @ temporarily suppresses errors of missing values
+
+@$startN = $_GET['startlat'];
+@$startE = $_GET['startlon'];
+
+@$endN = $_GET['endlat'];
+@$endE = $_GET['endlon'];
+
+@$size = $_GET['size'];
 
 
+$startN = 60; $startE = 24; $endN = 65; $endE = 27; $size = 1; // debug data
+
+
+
+$helpN = 0;
+$helpE = 0;
+
+
+// Horizontal lines
+
+$helpE = $startE;
+while ($helpE <= $endE)
+{
+	echo "$startN, $helpE;\n";
+	$helpE = $helpE + $size;
+}
