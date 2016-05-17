63e22710e457eabb9a1cdb15b2adc759da393795
Mikko Heikkinen <hawk@biomi.org>
2016-05-17 13:27:02 +0300

    Horizontal lines as KML

--- gridcreator/index.php
+++ gridcreator/index.php
@@ -12,6 +12,8 @@ Input:
 
 // @ temporarily suppresses errors of missing values
 
+$debug = FALSE;
+
 @$startN = $_GET['startlat'];
 @$startE = $_GET['startlon'];
 
@@ -21,24 +23,76 @@ Input:
 @$size = $_GET['size'];
 
 
-$startN = 60; $startE = 24; $endN = 65; $endE = 27; $size = 1; // debug data
+$startN = 60; $startE = 22; $endN = 65; $endE = 27; $size = 1; // debug data
 
 
 
 $helpN = 0;
 $helpE = 0;
 
+$tempPolygon = "";
+
+$masterKML = "";
 
 // Horizontal lines
+
+// KML polygon format: 24.1,60.1,0 24.2,60.1,0 24.2,60.2,0 24.1,60.2,0 24.1,60.1,0 
+
 
 $helpN = $startN;
 while ($helpN <= $endN)
 {
+	$tempPolygon = "";
 	$helpE = $startE;
 	while ($helpE <= $endE)
 	{
-		echo "$helpN, $helpE;\n";
+		$tempPolygon .= "$helpE,$helpN,0 ";
 		$helpE = $helpE + $size;
 	}
+
+	addPolygon($tempPolygon);
 	$helpN = $helpN + $size;
 }
+
+function addPolygon($polygon)
+{
+	global $masterKML;
+
+	$partialKML = "
+		<Placemark id=\"poly\">
+			<name>Polygon</name>
+			<styleUrl>#gridCreatorStyle</styleUrl>
+			<LineString>
+				<coordinates>
+					" . $polygon . " 
+				</coordinates>
+			</LineString>
+		</Placemark>
+	";
+
+	$masterKML .= $partialKML;
+}
+
+$debug = FALSE;
+
+if (! $debug)
+{
+	header('Content-type: application/vnd.google-earth.kml+xml');
+	header('Content-Disposition: attachment; filename="grid.kml"');
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+}
+
+?>
+
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+	<name>Grid</name>
+	<open>1</open>
+	<description>Made with KML Grid Creator</description>
+	<Folder>
+		<name>Grid</name>
+		<open>1</open>
+		<?php echo $masterKML; ?>
+	</Folder>
+</Document>
+</kml>
