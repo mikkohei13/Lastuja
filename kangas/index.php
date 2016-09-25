3758ca9a9ceb9882f5429fc8de7c17673984d4db
Mikko Heikkinen <hawk@biomi.org>
2016-09-25 09:22:03 +0300

    Learning canvas js, init with randomly moving element

---
+++ kangas/index.php
@@ -0,0 +1,38 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Kangas</title>
+        <meta name="description" content="Learning canvas.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/app.css">
+
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <div id="kangas">
+            <span id="elio"></span>
+        </div>
+
+<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> --><!-- Removed: latest not available -->
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.1.1.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
---
+++ kangas/js/main.js
@@ -0,0 +1,63 @@
+
+var w = $( window ).width();
+var h = $( window ).height();
+
+$( "#kangas" ).css( "width", w );
+$( "#kangas" ).css( "height", h );
+
+var x = w-20; // object size
+var y = h-20;
+
+var d = $( "#elio" ).width();
+var r = d / 2;
+
+console.log(x);
+console.log(y);
+
+
+var i = 0;
+var dur = 1500;
+
+while (i < 30)
+{
+  goToRandom(dur - (i * 50));
+  i++;
+}
+
+/*
+while (i < 10)
+{
+  dur = 1500 - (i * 100);
+
+  $( "#elio" ).animate({
+      top: y,
+      left: x,
+  }, dur);
+  $( "#elio" ).animate({
+      top: y,
+      left: 0,
+  }, dur);
+  $( "#elio" ).animate({
+      top: 0,
+      left: x,
+  }, dur);
+  $( "#elio" ).animate({
+      top: 0,
+      left: 0,
+  }, dur);
+
+  i++;
+
+}
+*/
+
+function goToRandom(durLocal)
+{
+  $( "#elio" ).animate({
+      top: Math.random() * y,
+      left: Math.random() * x,
+  }, durLocal);
+  console.log("duration: " + durLocal);
+
+//  var klon = $("#elio").clone().prop('id', 'klon');
+}
