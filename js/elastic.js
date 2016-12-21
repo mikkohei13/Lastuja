0eceb89fe41b3b9dba1da628f4b20e5c1ea1cba1
Mikko Heikkinen <hawk@biomi.org>
2016-12-21 23:11:57 +0200

    Fetch record count per species

--- js/elastic.js
+++ js/elastic.js
@@ -2,23 +2,29 @@
 console.log("Elastic");
 console.log("=====================");
 
-$(document).ready(function() {
 
+$("#species").blur(function() {
+	let species = $("#species").val();
+//	console.log(species);
+	getSpecies(species);
+});
+
+function getSpecies(species) {
 	$.ajax({
 		method: "GET",
-		url: "http://192.168.56.10:9200/gbif5/_search?q=species:Somateria%20mollissima",
+		url: "http://192.168.56.10:9200/gbif5/_search?q=species:" + species,
 		crossDomain: true,
 		beforeSend: function (xhr) {
 		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
 		}
 	})
-	.done(function( data ) {
-		console.log( data );
-		$("#total").text(data.hits.total);
+	.done(function(data) {
+//		console.log( data );
+		let count = data.hits.total;
+		let countFormatted = count.toLocaleString();
+		$("#total").text(countFormatted);
 	});
-
-});
-
+}
 
 /*
 
