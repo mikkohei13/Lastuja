6ba4aab48b4e8ebece1836513886b525ce58720a
Mikko Heikkinen <hawk@biomi.org>
2016-12-22 20:20:28 +0200

    Fetching results per species from Elastic

--- js/elastic.js
+++ js/elastic.js
@@ -3,16 +3,49 @@ console.log("Elastic");
 console.log("=====================");
 
 
-$("#species").blur(function() {
-	let species = $("#species").val();
-//	console.log(species);
-	getSpecies(species);
+$(document).ready(function() {
+	$("#query").text("Total");
+	getAll();
+});
+
+$("#species").keypress(function(event) {
+	if (event.which == 13) {
+		let species = $("#species").val();
+		if (species == "")
+		{
+			$("#query").text("Total");
+			getAll();
+		}
+		else
+		{
+			$("#query").text(species);
+			getSpecies(species);
+		}
+//		console.log(species);
+	}
 });
 
 function getSpecies(species) {
 	$.ajax({
 		method: "GET",
-		url: "http://192.168.56.10:9200/gbif5/_search?q=species:" + species,
+		url: "http://192.168.56.10:9200/baltic-aves/_search?q=species:" + species,
+		crossDomain: true,
+		beforeSend: function (xhr) {
+		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
+		}
+	})
+	.done(function(data) {
+//		console.log( data );
+		let count = data.hits.total;
+		let countFormatted = count.toLocaleString();
+		$("#total").text(countFormatted);
+	});
+}
+
+function getAll(species) {
+	$.ajax({
+		method: "GET",
+		url: "http://192.168.56.10:9200/baltic-aves/_search",
 		crossDomain: true,
 		beforeSend: function (xhr) {
 		    xhr.setRequestHeader ("Authorization", "Basic " + btoa("elastic" + ":" + "changeme"));
@@ -28,6 +61,16 @@ function getSpecies(species) {
 
 /*
 
+Javascript-based UI for making basic analysis of the data
+
+Species:
+Record count: absolute & proportion of class/order
+Year histogram: absolute & relative to class/order
+Month histogram of all years
+
+Map:
+Species on a map, clustered
+Species count of selected taxon on a map, clustered
 
 
 */
