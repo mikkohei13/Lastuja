4772f2656929805c384053af66d6891fd6d825bb
Mikko Heikkinen <hawk@biomi.org>
2017-01-07 23:38:30 +0200

    Reading ulr, cleanups

--- node/app/index.js
+++ node/app/index.js
@@ -1,48 +1,45 @@
 
-const lajiAPI = require('./lajiapi.js');
-
-// Options
-const http = require('http');  
 const port = 3000;
 
-// Functions
-const requestListener = function listener(request, response) {
-  logToConsole(request);
+const lajiAPI = require('./lajiapi.js');
+const http = require('http');
+const url = require('url');
 
+// Functions
+const startServer = function startServer(err) {  
+  if (err) {
+    return console.log('Something went wrong.', err);
+  }
+  console.log(`Server is listening on port ${port}`);
+}
+
+const requestListener = function requestListener(request, response) {
   response.end('Hoi Node.js Server!');
+  logToConsole(request);
 }
 
 function logToConsole(request) {
-  if ("/favicon.ico" == request.url)
+  // Don't handle favicon requests
+  if ('/favicon.ico' != request.url)
   {
+    let urlObject = url.parse(request.url);
+    console.log(urlObject);
 
-  }
-  else
-  {
-    console.log(request.url);
-  //  console.log(request.headers);
-
-    let json = lajiAPI.importantVariable;
-    console.log(json);
+    if ("uploads" == request.url)
+    {
+      let json = lajiAPI.importantVariable;
+    }
   }
 }
 
+/*
+createServer is designed to call callbeack function defined in the variable "requestListener"
+by givingh it two arguments, request and reponse objects. This is the standard way of using 
+callback functions.
+*/
 
-
-
-// createServer is designed to call callbeack function defined in the variable "requestListener" by givingh it two arguments, request and reponse objects. This is the standard way of use callback functions.
 const server = http.createServer(requestListener); 
-
-server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-
-  console.log(`server is listening on ${port}`);
-});
-
-
-
+server.listen(port, startServer);
 
 
 // ---------------------------------------------------------------
