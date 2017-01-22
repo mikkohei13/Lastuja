b0a64ce80a2742b454b521b99faec7294e669f24
Mikko Heikkinen <hawk@biomi.org>
2017-01-22 22:24:25 +0200

    Add Ownmaps

---
+++ js/ownmaps/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Ownmaps</title>
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<link rel="stylesheet" href="media/app.css" media="all" />
+		<link rel="stylesheet" href="node_modules/leaflet/dist/leaflet.css" media="all" />
+
+	    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+	    <script src="node_modules/leaflet/dist/leaflet.js"></script>
+
+	</head>
+	<body>
+		<header>
+		</header>
+		<div id="content">
+			<div id="mymap"></div>
+		</div>
+
+		<script src="keys.js"></script>
+		<script src="app.js"></script>
+	</body>
+</html>
