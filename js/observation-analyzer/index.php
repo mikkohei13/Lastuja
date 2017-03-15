3e568a30b0452cd6205361e0f95b0b665cf8b739
Mikko Heikkinen <hawk@biomi.org>
2017-03-15 19:58:53 +0200

    init Observation Analyzer

---
+++ js/observation-analyzer/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Observation Analyzer</title>
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="stylesheet" href="media/app.css?c=<?php echo rand(0,10000); ?>" media="all" />
+	    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+
+	</head>
+	<body>
+		<header>
+		</header>
+		<div id="content">
+		</div>
+
+		<script src="keys.js"></script>
+		<script src='app.js<?php echo rand(0,10000); ?>'></script>;
+	</body>
+</html>
