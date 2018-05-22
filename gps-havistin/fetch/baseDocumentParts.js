71191a6722e1206dc919658697ca522b4f799bb3
Mikko Heikkinen <hawk@biomi.org>
2018-05-22 22:04:22 +0200

    Linting baseDocumentParts.js, closes #1

--- gps-havistin/fetch/baseDocumentParts.js
+++ gps-havistin/fetch/baseDocumentParts.js
@@ -1,80 +1,80 @@
 
-let baseDocument = {
-  "creator": "MA.3",
-  "gatheringEvent": {
-    "legPublic": true,
-    "leg": [
-      "MA.3"
+const baseDocument = {
+  creator: "MA.3",
+  gatheringEvent: {
+    legPublic: true,
+    leg: [
+      "MA.3",
     ],
-    "dateBegin": "" // date here
+    dateBegin: "", // date here
   },
-  "formID": "JX.519",
-  "editors": [
-    "MA.3"
+  formID: "JX.519",
+  editors: [
+    "MA.3",
   ],
-  "secureLevel": "MX.secureLevelNone",
-  "gatherings": [
+  secureLevel: "MX.secureLevelNone",
+  gatherings: [
     {
-      "units": [
-          // units here
+      units: [
+        // units here
       ],
-      "geometry": {
-        "type": "GeometryCollection",
-        "geometries": [
-            // geometries here
-        ]
+      geometry: {
+        type: "GeometryCollection",
+        geometries: [
+          // geometries here
+        ],
       },
-      "notes": "" // Track name here
-    }
+      notes: "", // Track name here
+    },
   ],
-  "keywords": [
+  keywords: [
     "havistin",
-    "gpx"
+    "gpx",
   ],
-  "publicityRestrictions": "MZ.publicityRestrictionsPublic"
+  publicityRestrictions: "MZ.publicityRestrictionsPublic",
 };
 
-let baseUnit = {
-    "recordBasis": "MY.recordBasisHumanObservation",
-    "taxonConfidence": "MY.taxonConfidenceSure",
-    "count": "1",
-    "identifications": [
-      {
-        "taxon": ""  // taxon here
-      }
-    ],
-    "unitGathering": {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          18.24526,
-          59.328291
-        ]
-      }
-    }
-  };
+const baseUnit = {
+  recordBasis: "MY.recordBasisHumanObservation",
+  taxonConfidence: "MY.taxonConfidenceSure",
+  count: "1",
+  identifications: [
+    {
+      taxon: "", // taxon here
+    },
+  ],
+  unitGathering: {
+    geometry: {
+      type: "Point",
+      coordinates: [
+        18.24526,
+        59.328291,
+      ],
+    },
+  },
+};
 
-let baseGeometry = {
-    "type": "LineString",
-    "coordinates":
+const baseGeometry = {
+  type: "LineString",
+  coordinates:
     [
-        [
+      [
         18.252975,
-        59.326329
-        ],
-        [
+        59.326329,
+      ],
+      [
         18.25186,
-        59.326136
-        ],
-        [
+        59.326136,
+      ],
+      [
         18.250659,
-        59.325532
-        ]
-    ]
+        59.325532,
+      ],
+    ],
 };
 
 module.exports = {
-    "baseDocument" : baseDocument,
-    "baseGeometry" : baseGeometry,
-    "baseUnit" : baseUnit
+  baseDocument,
+  baseGeometry,
+  baseUnit,
 };
